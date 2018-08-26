da711d22282f215df0ac72d9cf45628a1a297525
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-27 01:49:19 +0600

    Create 2.increment.js

---
+++ 2.increment.js
@@ -0,0 +1,20 @@
+// increment
+var i = 5;
+i++;//i=i+1
+console.log(i);
+
+// pre & post increment
+var a,b;
+var x = 4;
+a = x++;//x=y then y=y+1
+b = ++x;//y=y+1 then y=y
+console.log(a,b);
+
+// compound assignment
+var n=2;
+//n+=2;//n=n+2
+//n-=2;//n=n-2
+//n*=2;//n=n*2
+//n/=2;//n=n/2
+//n%=2;//n=n%2
+console.log(n);
