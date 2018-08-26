bd6313f33b3be41a6c83a26ee76632cecf428d08
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-27 01:50:12 +0600

    Create 4.condition.js

---
+++ 4.condition.js
@@ -0,0 +1,32 @@
+// comparison operator -smaller or greater
+// var n=10;
+// if(n<10){
+//     console.log(n,'is smaller than 10');
+// }else if(n==10){
+//     console.log(n,'is equal to 10');
+// }
+// else{
+//     console.log(n,'is greater than 10');
+// }
+
+
+// if condition -jamal & kamal
+var personOne = 'Jamal';
+var personTwo = 'Kamal';
+var areTheyBrother = true;
+// if('Jamal'==personOne){
+//     if('Kamal'==personTwo){
+//         if(areTheyBrother){
+//             console.log("jamal & kamal are brother");
+//         }else{
+//             console.log("jamal & kamal are not brother");
+//         }
+//     }
+// }
+
+// if condition -jamal & kamal multiple statement
+if("Jamal"==personOne && "Kamal"==personTwo && areTheyBrother){
+    console.log('Jamal & Kamal are brother');
+}else{
+    console.log('They are not brother');
+}
