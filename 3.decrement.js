aa756ae7c73222b08988fab9db319e70a6149d65
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-27 01:49:39 +0600

    Create 3.decrement.js

---
+++ 3.decrement.js
@@ -0,0 +1,16 @@
+// decrement
+var i = 5;
+i--;//i=i-1
+console.log(i);
+
+// pre & post decrement
+var a,b;
+var x = 5;
+a=x--;//a=x then x=x-1
+b=--x;//x=x-1 then b=x
+console.log(a,b);
+
+// compound assignment
+var n=2;
+n+=2;//n=n+2
+console.log(n);
