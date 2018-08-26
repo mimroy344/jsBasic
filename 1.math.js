3d4d9d9e34b752ec04d550ff4c2b074a36739647
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-27 01:48:52 +0600

    Create 1.math.js

---
+++ 1.math.js
@@ -0,0 +1,22 @@
+// cost of eggs -calculate
+var eggPrice =9;
+var numberOfEggs = 4;
+var totalCostOfEggs = eggPrice*numberOfEggs;
+console.log('Total',totalCostOfEggs,'taka');
+
+// area of circle -calculate
+var radius = 7;
+const pi = 3.14159;
+var areaOfCircle = pi *(radius*radius);
+console.log('Area of Circle',areaOfCircle,'inch');
+
+// usd to bdt -calculate
+var usdToBdt = 83.5;
+var bdt = 5000;
+var usd = bdt/usdToBdt;
+console.log('$',usd);
+
+// remainder
+var n = 4;
+var remainder = n%2;
+console.log(remainder);
