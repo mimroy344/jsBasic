32d1cdcb00527f50f7512bbaf83eac8db8682474
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-27 01:51:46 +0600

    Create 8.ternary.js

---
+++ 8.ternary.js
@@ -0,0 +1,10 @@
+// ternary
+var n = 3;
+var remainder = n%2;
+var result;
+
+// result = (0==remainder)? 'even':'odd';
+// console.log(result);
+
+result = (0<n)?'positive':'negative';
+console.log('This number',result);
