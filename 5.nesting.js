78e0f5f139b53f4c290ebf0227f9f6d162b4e0c0
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-27 12:41:19 +0600

    Update 5.nesting.js

--- 5.nesting.js
+++ 5.nesting.js
@@ -42,3 +42,59 @@
 // }else{
 //     console.log(n,'is odd');
 // }
+
+
+
+
+// prime number -2,3,5,7
+/*var n = 99;
+
+if(n<10){
+    if(2==n||3==n||5==n||7==n){
+        console.log(n,'is prime number & smaller than 10');
+    }else{
+        console.log(n,'is not prime number & smaller than 10');
+    }
+}else{
+    console.log(n,'is greater than 10')
+}*/
+
+
+
+// nesting multiple statement -prime number
+/*var n = 11;
+if(n<=10 && (2==n||3==n||5==n||7==n)){
+    console.log(n,'is prime & smaller than 10');
+}else if(n<=10){
+    console.log(n,'is not prime but smaller than 10');
+}else{
+    console.log(n,'is greater than 10');
+}*/
+
+
+
+
+// positive and negative number
+/*var n =3;
+if(n>0){
+    console.log(n,'is positive number.');
+}else if(0==n){
+    console.log(n,'is natural number.');
+}else{
+    console.log(n,'is negative number.')
+}*/
+
+
+
+// even and odd
+var n =11;
+var result;
+var remainder=n%2;
+if(0==n){
+    console.log(n,'is natural number.');
+}else if(0==remainder){
+    console.log(n,'is even number.');
+}else{
+    console.log(n,'is odd number.');
+}
+
