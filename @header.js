6a026223622701c60600a36ec9a8bae870652211
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-27 12:36:20 +0600

    Create @header.js

---
+++ @header.js
@@ -0,0 +1,32 @@
+/*
+//1.math
+a.cost of eggs --calculator
+b.area of circle --calculator
+c.usd to bdt --calculator
+d.remainder --show
+
+//2.increment & compound assignment operator
+a.pre & post increment --compound assignment operaotr
+
+//3.decrement & compound assignment operator
+a.pre & post decrement
+
+//4.condition
+a.comparison operator --smaller or greater
+b.if conditional statement --jamal&Kamal nesting
+c.if multiple statement check--they are brother
+
+//5.nesting
+a.if nesting  --prime number
+b.positive & negative number
+c.even & odd 
+
+//6.movie
+a.movie Rating --calculate
+
+//7.age -calculate
+a.your age to condition--adult ,teenager,baby
+
+//8.ternary
+a.ternary operator --even,positive
+*/
