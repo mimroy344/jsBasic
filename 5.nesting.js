97f5285313cd5d5b6705acfd7aa544def9022f24
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-27 12:54:32 +0600

    Update 5.nesting.js

--- 5.nesting.js
+++ 5.nesting.js
@@ -1,51 +1,3 @@
-// nesting - 2,3,5,7
-// var n = 7;
-// var result;
-// if(n<10){
-//     if(2==n||3==n||5==n||7==n){
-//         result = n+' is smaller than 10 and its prime number'; 
-//     }else{
-//         result = n+' is smaller than 10';
-//     }
-// }else{
-//     result = n+' is greater than 10';
-// }
-
-
-
-// if((n<10)&&(2==n||3==n||5==n||7==n)){
-//     result = n+' is smaller than 10 and its prime number';
-// }else if(n<10){
-//     result = n+' is smaller than 10'
-// }else{
-//     result = n+' is greater than 10';
-// }
-// console.log(result);
-
-// positive number
-// var n = -0;
-// if(n>0){
-//     console.log(n,'is positive number');
-// }else if(0==n){
-//     console.log(n,'is natural number');
-// }
-// else{
-//     console.log(n,'is negative number');
-// }
-
-
-// even or odd
-// var n = 6;
-// var remainder = n%2;
-// if(0==remainder){
-//     console.log(n,'is even');
-// }else{
-//     console.log(n,'is odd');
-// }
-
-
-
-
 // prime number -2,3,5,7
 /*var n = 99;
 
@@ -72,6 +24,16 @@ if(n<=10 && (2==n||3==n||5==n||7==n)){
 }*/
 
 
+// positive number
+// var n = -0;
+// if(n>0){
+//     console.log(n,'is positive number');
+// }else if(0==n){
+//     console.log(n,'is natural number');
+// }
+// else{
+//     console.log(n,'is negative number');
+// }
 
 
 // positive and negative number
@@ -83,7 +45,6 @@ if(n>0){
 }else{
     console.log(n,'is negative number.')
 }*/
-
 
 
 // even and odd
