5cb4f9e5121d94a464e1dbb3441e1e0f2114003e
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-27 12:40:33 +0600

    Update 4.condition.js

--- 4.condition.js
+++ 4.condition.js
@@ -30,3 +30,44 @@ if("Jamal"==personOne && "Kamal"==personTwo && areTheyBrother){
 }else{
     console.log('They are not brother');
 }
+
+
+
+
+
+// conditional statement
+/*var n = 10;
+
+if (n<10){
+    console.log('your number '+n+' is smaller than 10');
+}else if (n==10){
+    console.log('Your number '+n+' is equal to 10');
+} else {
+    console.log('your number '+n+' is greater than 10');
+}*/
+
+// if condition -nesting
+/*var personOne = 'Jamal';
+var personTwo = 'Kamal';
+var areTheyBrother = false;
+
+if('Jamal'==personOne){
+    if('Kamal'==personTwo){
+        if(areTheyBrother){
+            console.log(personOne,'and',personTwo,'are Brother');
+        }else{
+            console.log(personOne,'and',personTwo,'are not brother');
+        }
+    }
+}*/
+
+// if multiple statement -cheeck
+/*var personOne = 'Jamal';
+var personTwo = "Kamal";
+var areTheyBrother = false;
+
+if ("Jamal"==personOne && 'Kamal'==personTwo && areTheyBrother){
+    console.log('Jamal & Kamal are Brother.');
+}else{
+    console.log('they are not brother.')
+}*/
